13d6680956d63137fde4aab1e645962a675a8525
Çağatay Yıldız <yildiz.cagatay@hotmail.com>
2024-07-02 11:45:12 +0300

    Add project files.

---
+++ Assistt.Infrastructure/EF/AssisttContext.cs
@@ -0,0 +1,20 @@
+﻿using Assistt.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assistt.Infrastructure.EF
+{
+    public class AssisttContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           optionsBuilder.UseSqlServer("Server=.;Database=AssisttDb;Trusted_Connection=True;trustServerCertificate=True");
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
