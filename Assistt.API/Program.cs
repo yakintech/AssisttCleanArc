554ac31c22bd371a15f10e56aabdf92f449e4f2f
Çağatay Yıldız <yildiz.cagatay@hotmail.com>
2024-07-02 14:41:50 +0300

    fluent validation added

--- Assistt.API/Program.cs
+++ Assistt.API/Program.cs
@@ -1,13 +1,18 @@
 using Assistt.Application.Commands;
+using Assistt.Application.DTO;
+using Assistt.Application.Validators.Products;
 using Assistt.Infrastructure.EF;
 using Assistt.Infrastructure.Repositories;
 using Assistt.Infrastructure.UnitOfWork;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+    ;
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -22,7 +27,11 @@ builder.Services.AddMediatR(opt =>
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
+builder.Services.AddScoped<IValidator<CreateProductDto>, CreateProductValidator>();
+
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+builder.Services.AddFluentValidationAutoValidation();
 
 
 var app = builder.Build();
---
+++ Assistt.Application/Validators/Products/CreateProductValidator.cs
@@ -0,0 +1,20 @@
+﻿using Assistt.Application.DTO;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assistt.Application.Validators.Products
+{
+    public class CreateProductValidator : AbstractValidator<CreateProductDto>
+    {
+        public CreateProductValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("Name alani bos gecilemez");
+        }
+    }
+}
